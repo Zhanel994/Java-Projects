23c1805e5e200c164869dc6a775d230c2f21c273
ТойшыбековаЖанель <zhaneltoyshybekova@gmail.com>
2025-07-12 12:12:33 +0500

    Created class Task and added two constructors.

--- to-do-list/src/application/com/entities/Task.java
+++ to-do-list/src/application/com/entities/Task.java
@@ -1,4 +1,89 @@
 package application.com.entities;
 
+import java.time.LocalDate;
+
 public class Task {
+    private int id;
+    private String title;
+    private String executor;
+    private String description;
+    private String status;
+    private LocalDate creationDate;
+    private LocalDate completionDate;
+
+    public Task(int id, String title, String executor, String description, String status, LocalDate creationDate, LocalDate completionDate) {
+        this.id = id;
+        this.title = title;
+        this.executor = executor;
+        this.description = description;
+        this.status = status;
+        this.creationDate = creationDate;
+        this.completionDate = completionDate;
+    }
+
+    public Task(int id, String title, String executor, String description) {
+        this.id = id;
+        this.title = title;
+        this.executor = executor;
+        this.description = description;
+        this.status = "new";
+        this.creationDate = LocalDate.now();
+        this.completionDate = null;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getExecutor() {
+        return executor;
+    }
+
+    public void setExecutor(String executor) {
+        this.executor = executor;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public LocalDate getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDate creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public LocalDate getCompletionDate() {
+        return completionDate;
+    }
+
+    public void setCompletionDate(LocalDate completionDate) {
+        this.completionDate = completionDate;
+    }
 }
