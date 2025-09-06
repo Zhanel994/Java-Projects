23f5b154b21b9c7131456b5d601fb5ff82ac872d
ТойшыбековаЖанель <zhaneltoyshybekova@gmail.com>
2025-09-06 13:31:00 +0500

    added: clearing the storage function.

--- exam_8_zhanel_toishybekova/src/main.js
+++ exam_8_zhanel_toishybekova/src/main.js
@@ -3,10 +3,15 @@ const addTaskBtn = document.getElementById("addTaskBtn");
 const toDoTask  = document.getElementById("toDoTask");
 const inProgressTask = document.getElementById("inProgressTask");
 const doneTask = document.getElementById("doneTask");
-const clearStorage = document.getElementById("clearStorageBtn");
+const clearStorageBtn = document.getElementById("clearStorageBtn");
 
-const tasks = localStorage.getItem("tasks") ? JSON.parse(localStorage.getItem("tasks")) : [];
+let tasks = localStorage.getItem("tasks") ? JSON.parse(localStorage.getItem("tasks")) : [];
 
 function generateId() {
     return Math.floor(Math.random() * 100);
 }
+
+clearStorageBtn.onclick = function() {
+    localStorage.clear();
+    tasks = [];
+};
