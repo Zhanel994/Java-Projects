bceb0d0a31566c458f182a8d88bc6c0c37bd5b4d
ТойшыбековаЖанель <zhaneltoyshybekova@gmail.com>
2025-09-16 11:00:05 +0500

    added: Product repository.

---
+++ month9-onlineshop/src/main/java/application/month9onlineshop/com/repositories/ProductRepository.java
@@ -0,0 +1,7 @@
+package application.month9onlineshop.com.repositories;
+
+import application.month9onlineshop.com.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
